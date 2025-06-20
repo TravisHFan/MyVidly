747fae26bf8304246fd5428fa844a580a0408c07
TravisHFan <travisfanht@gmail.com>
2025-06-20 16:26:57 -0700

    Add the test for routes/authen.js

--- tests/integration/authen.test.js
+++ tests/integration/authen.test.js
@@ -1,4 +1,5 @@
 const request = require("supertest");
+const bcrypt = require("bcrypt");
 const app = require("../../app"); // Adjust the path as necessary
 const { User } = require("../../models/user"); // Ensure User model is loaded
 
@@ -30,5 +31,52 @@ describe("authen middleware", () => {
   it("should return 200 if token is valid", async () => {
     const res = await exec();
     expect(res.status).toBe(200);
+  });
+});
+
+describe("/api/authen", () => {
+  let email;
+  let password;
+
+  const exec = async () => {
+    return await request(app).post("/api/authen").send({ email, password });
+  };
+
+  beforeEach(async () => {
+    email = "user1@example.com";
+    password = "12345";
+    const salt = await bcrypt.genSalt(10);
+    const hashed = await bcrypt.hash(password, salt);
+    await new User({ name: "user1", email, password: hashed }).save();
+  });
+
+  it("should return 400 if email is invalid", async () => {
+    email = "1234";
+    const res = await exec();
+    expect(res.status).toBe(400);
+  });
+
+  it("should return 400 if password is less than 5 characters", async () => {
+    password = "1234";
+    const res = await exec();
+    expect(res.status).toBe(400);
+  });
+
+  it("should return 400 if user with given email does not exist", async () => {
+    email = "notexist@example.com";
+    const res = await exec();
+    expect(res.status).toBe(400);
+  });
+
+  it("should return 400 if password is incorrect", async () => {
+    password = "wrongpassword";
+    const res = await exec();
+    expect(res.status).toBe(400);
+  });
+
+  it("should return a token if credentials are valid", async () => {
+    const res = await exec();
+    expect(res.status).toBe(200);
+    expect(res.text).toBeTruthy();
   });
 });
